234deec815309f481d0b073eadb1305da9bbe9ce
JIHOJU <jihoju96@gmail.com>
2022-01-30 17:28:17 +0900

    feat: add heatwave_casualities

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -1,8 +1,20 @@
-import pprint
-
-from django.shortcuts import render
+from api_module.shelter import check_place
 from django.http import HttpResponse
-from module.shelter import check_place
+from api_module import heatwave_casualties_region
+
+
 def index(request):
     res = check_place()
     return HttpResponse(res)
+
+
+def heatwave_total(request):
+    data = heatwave_casualties_region.heatwave_casualties_total()
+
+    return HttpResponse(data)
+
+
+def heatwave_region(request):
+    data = heatwave_casualties_region.heatwave_casualties_region()
+
+    return HttpResponse(data)
