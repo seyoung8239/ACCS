182c19bff09b0c933ac2b3294d20c3fdb034d438
JangYunSeong <46878756+JangYunSeong@users.noreply.github.com>
2022-01-31 23:06:48 +0900

    backend : 2021.01.31

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from .views import heatwave_total, heatwave_region
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    # path('', views.index),
+    path('heatwave/total', heatwave_total),
+    path('heatwave/region', heatwave_region)
 ]
--- backend/backend/views.py
+++ backend/backend/views.py
@@ -3,6 +3,18 @@ import pprint
 from django.shortcuts import render
 from django.http import HttpResponse
 from module.shelter import check_place
+from module.heatwave_casualties_region import heatwave_casualties_region, heatwave_casualties_total
 def index(request):
     res = check_place()
     return HttpResponse(res)
+
+def heatwave_total(request):
+    data = heatwave_casualties_total()
+
+    return HttpResponse(data)
+
+
+def heatwave_region(request):
+    data = heatwave_casualties_region()
+
+    return HttpResponse(data)
