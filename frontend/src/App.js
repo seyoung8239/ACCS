760649a020d9f094f7c6e6679459224163cd9755
seyoung8239 <seyoung8239@gmail.com>
2022-02-01 00:46:49 +0900

    update react-router changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 import Appbar from './layouts/Appbar';
 import HSTable from './pages/HSTable';
@@ -9,14 +9,12 @@ function App() {
   return (
     <>
       <Appbar />
-      <Switch>
-        <Route exact path='/'>
-          <Redirect to='/map' />
-        </Route>
-        <Route path="/map" component={TempMap} />
-        <Route path="/table" component={HSTable} />
-        <Route path="/board" component={ReqBoard} />
-      </Switch>
+      <Routes>
+        <Route path="/map" element={<TempMap />} />
+        <Route path="/table" element={<HSTable />} />
+        <Route path="/board" element={<ReqBoard />} />
+        <Route path="*" element={<Navigate to="/map" />} />
+      </Routes>
     </>
   );
 }
