b7bb8135c277f048324ed25d2b51c440666d478a
seyoung8239 <seyoung8239@gmail.com>
2022-02-01 00:33:27 +0900

    add page router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,23 @@
+import { Redirect, Route, Switch } from 'react-router-dom';
+
+import Appbar from './layouts/Appbar';
+import HSTable from './pages/HSTable';
+import ReqBoard from './pages/ReqBoard';
+import TempMap from './pages/TempMap';
+
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <>
+      <Appbar />
+      <Switch>
+        <Route exact path='/'>
+          <Redirect to='/map' />
+        </Route>
+        <Route path="/map" component={TempMap} />
+        <Route path="/table" component={HSTable} />
+        <Route path="/board" component={ReqBoard} />
+      </Switch>
+    </>
   );
 }
 
