977091a913f4cec41bb3ac8f09e6f90e27b4676e
JIHOJU <jihoju96@gmail.com>
2022-02-01 18:08:40 +0900

    feat: add inquiry_response_heatwave by field

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -1,8 +1,5 @@
-from api_module.shelter import check_place
 from django.http import HttpResponse
-from api_module import heatwave_casualties_region, shelter
-from django.http import HttpResponse
-from api_module import shelter, heatwave_casualties_region
+from api_module import shelter, heatwave_casualties_region, inquiry_response_heatwave
 
 
 def index(request):
@@ -18,5 +15,13 @@ def heatwave_total(request):
 
 def heatwave_region(request):
     data = heatwave_casualties_region.heatwave_casualties_region()
+
+    return HttpResponse(data)
+
+
+def inquiry_response_heatwave_by_field(request, field):
+    """ 분야(field)별 폭염 대응 기준 조회 """
+
+    data = inquiry_response_heatwave.get_action_by_field(field)
 
     return HttpResponse(data)
