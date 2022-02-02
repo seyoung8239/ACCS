1215668d912e5251f2742145ea42ebd4acee2bda
seyoung8239 <seyoung8239@gmail.com>
2022-02-02 09:19:31 +0900

    add post cards on board page

--- frontend/src/pages/ReqBoard/index.js
+++ frontend/src/pages/ReqBoard/index.js
@@ -1,6 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { initializeApp } from "firebase/app";
-import {getFirestore} from 'firebase/firestore';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Container from '@material-ui/core/Container';
+import useStyles from './styles';
+
+import BoardCard from '../../component/BoardCard';
 
 const firebaseConfig = {
   apiKey: "AIzaSyASbSNdJYPNqQWkERYQC4adD_x-nSsmL2E",
@@ -13,12 +20,36 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore();
+const db = getFirestore(app);
 
 const ReqBoard = () => {
+  const classes = useStyles();
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setPosts([]);
+      const querySnapshot = await getDocs(collection(db, 'board'));
+      querySnapshot.forEach((doc) => {
+        setPosts(p => p.concat(doc.data()));
+      });
+    }
+    fetchPosts();
+  }, []);
+
   return (
     <>
-      ReqBoard
+      {console.log(posts)}
+      <Box className={classes.box} />
+      <Container>
+        <Grid container spacing={2}>
+          {posts.map((post, i) => (
+            <Grid item xs={4}>
+              <BoardCard post={post} key={i} />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
     </>
   )
 }
--- frontend/src/pages/ReqBoard/styles.js
+++ frontend/src/pages/ReqBoard/styles.js
@@ -1,7 +1,9 @@
-import {makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(()=> ({
-
+const useStyles = makeStyles(() => ({
+  box: {
+    height: '100px'
+  }
 }));
 
 export default useStyles;
