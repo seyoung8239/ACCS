250f50dcdd251bc3ce99a039d8fa7adf5ac9fda2
seyoung8239 <seyoung8239@gmail.com>
2022-02-05 00:56:18 +0900

    add api route file

---
+++ frontend/src/RouteAPI/index.js
@@ -0,0 +1,3 @@
+export const shelter = "http://34.64.174.66:8000/shelter";
+export const heatwave_total = "http://34.64.174.66:8000/heatwave/total";
+export const heatwave_region = "http://34.64.174.66:8000/heatwave/region";
