99e2d74f51b2f39587956f710cba5fe39e6a8bc6
장윤성 <46878756+JangYunSeong@users.noreply.github.com>
2022-02-04 22:39:03 +0900

    feat: shelter position

--- backend/api_module/shelter.py
+++ backend/api_module/shelter.py
@@ -12,6 +12,7 @@ serviceKey = 'eevsrrwRji5TqX4eCcvWHfSfq1ObEwH0L0/nYNIoWoZIizEPi4Qf7N2vQ8D1CC6LLi
 serviceKeyDecoded = unquote(serviceKey, 'UTF-8')
 url = 'http://apis.data.go.kr/1741000/HeatWaveShelter2/getHeatWaveShelterList2'
 # url_Area = "http://apis.data.go.kr/1741000/HeatWaveShelter2/getHeatWaveShelterCrntStList2"
+FILE_PATH = "./api_json/shelter.json"
 
 def find_short(longitude, latitude):
     size = 3  # 가장가까운 쉼터 몇개 알려줄건지
@@ -120,88 +121,26 @@ def find_short(longitude, latitude):
 
 
 def check_place():
-    longitude = 126.96733695359828
-    latitude = 37.58368071740475
-    # 37.58368071740475, 126.96733695359828 / 서울 종로구 35.818969493056365, 128.53843740548325 / 대구 달서구 상인동
-    # 35.804066, 128.500381
-    areaNumber = make_code(longitude, latitude)
-    areaAdd = make_address(longitude, latitude)
-    # print(areaAdd)
-    # print(areaNumber)
-    # for i in range(1,100): # 리 단위 구역은 반복문 돌려서 다 찾아야 될듯
-    #     if(i < 10):
-    #       areaNumber[0] = areaNumber[0,-2] + '0' + str(i)
-    #     else:
-    #      areaNumber[0] = areaNumber[0,-2] + str(i)
-    params = {'serviceKey': serviceKey, 'type': 'xml', "areaCd": areaNumber[0]}
-    response = requests.get(url, params=params)
-    content = response.content
-    # pp = pprint.PrettyPrinter(indent = 4)
-    # print(pp.pprint(content))
-
-    # subjects = {
-    #     resSeqNo: 시설번호
-    #     year: 년도
-    #     areaCd: 지역코드
-    #     equptype: 시설유형
-    #     restname: 쉼터명
-    #     restaddr: 소재지지번주소
-    #     creDttm: 생성일시
-    #     updtDttm: 수정일시
-    #     useYn: 사용여부
-    #     areaNm: 지역명
-    #     operBeginDe: 운영시작일자
-    #     operEndDe: 운영종료일자
-    #     ar: 시설면적
-    #     colrHoldElefn : 선풍기보유대수
-    #     usePsblNmpr : 이용가능인원수
-    #     colrHoldArcndtn : 에어컨보유대수
-    #     chckMatterNightOpnAt: 야간개방
-    #     chckMatterWkendHdayOpnAt: 휴일개방
-    #     chckMatterStayngPsblAt: 숙박가능여부
-    #     rm:특이사항
-    #     dltAdres:소재지도로명주소
-    #     mngdpt_cd:관리기관
-    #     mngdptCd:관리기관 전화번호
-    #     la:위도
-    #     lo:경도
-    # }
-    soup = BeautifulSoup(content, "lxml-xml")
-    rows = soup.find_all("row")
-    shelter = ""
-    for row in rows:
-        file_data = OrderedDict()
-        # file_data["restSeqNo"] = row.find('restSeqNo').get_text()
-        # file_data["year"] = row.find('year').get_text()
-        file_data["areaCd"] = row.find('areaCd').get_text()
-        # file_data["equptype"] = row.find('equptype').get_text()
-        file_data["restname"] = row.find('restname').get_text()
-        file_data["restaddr"] = row.find('restaddr').get_text()
-        # file_data["creDttm"] = row.find('creDttm').get_text()
-        # file_data["updtDttm"] = row.find('updtDttm').get_text()
-        file_data["useYn"] = row.find('useYn').get_text()
-        # file_data["areaNm"] = row.find('areaNm').get_text()
-        # file_data["operBeginDe"] = row.find('operBeginDe').get_text()
-        # file_data["operEndDe"] = row.find('operEndDe').get_text()
-        # file_data["ar"] = row.find('ar').get_text()
-        # file_data["colrHoldElefn"] = row.find('colrHoldElefn').get_text()
-        # file_data["usePsblNmpr"] = row.find('usePsblNmpr').get_text()
-        # file_data["colrHoldArcndtn"] = row.find('colrHoldArcndtn').get_text()
-        # file_data["chckMatterNightOpnAt"] = row.find('chckMatterNightOpnAt').get_text()
-        # file_data["chckMatterWkendHdayOpnAt"] = row.find('chckMatterWkendHdayOpnAt').get_text()
-        # file_data["chckMatterStayngPsblAt"] = row.find('chckMatterStayngPsblAt').get_text()
-        # file_data["rm"] = row.find('rm').get_text()
-        # file_data["dtlAdres"] = row.find('dtlAdres').get_text()
-        # file_data["mngdpt_cd"] = row.find('mngdpt_cd').get_text()
-        # file_data["mngdptCd"] = row.find('mngdptCd').get_text()
-        # file_data["xcord"] = row.find("xcord").get_text()
-        # file_data["ycord"] = row.find("ycord").get_text()
-        file_data["la"] = row.find('la').get_text()
-        file_data["lo"] = row.find('lo').get_text()
-        x = file_data["la"]
-        y = file_data["lo"]
-        if (file_data["useYn"] == "Y"):
-            shelter += json.dumps(file_data, ensure_ascii=False, indent="\t")
-            shelter += "\n"
-    # print(best_shelter)
+    pageNo = 1
+    shelter = []
+    for i in range(1, 497):
+        params = {'serviceKey': serviceKey, 'type': 'xml', 'pageNo' : str(pageNo), 'numOfRows' : '100'}
+        response = requests.get(url, params=params)
+        content = response.content
+        soup = BeautifulSoup(content, "lxml-xml")
+        rows = soup.find_all("row")
+        for row in rows:
+            file_data = OrderedDict()
+            file_data["restname"] = row.find('restname').get_text()
+            file_data["restaddr"] = row.find('restaddr').get_text()
+            file_data["useYn"] = row.find('useYn').get_text()
+            file_data["la"] = row.find('la').get_text()
+            file_data["lo"] = row.find('lo').get_text()
+            if file_data["useYn"] == "Y":
+                shelter.append(file_data)
+        pageNo += 1
+        print(pageNo)
+    with open(FILE_PATH, 'w') as outfile:
+        json.dump(shelter, outfile, ensure_ascii=False, indent='\t')
+    print('complete')
     return shelter
