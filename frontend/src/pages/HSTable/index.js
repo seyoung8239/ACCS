50c9a983b61d88e3c7af9676a76ec686e1f092ba
seyoung8239 <seyoung8239@gmail.com>
2022-02-04 17:29:18 +0900

    add HSTable page structure

--- frontend/src/pages/HSTable/index.js
+++ frontend/src/pages/HSTable/index.js
@@ -1,9 +1,79 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
+import useStyles from './styles';
 
 const HSTable = () => {
+  const classes = useStyles();
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get('http://34.64.174.66:8000/heatwave/total');
+        setData(res.data);
+      } catch (e) {
+        console.log(e);
+      }
+      setIsLoading(true);
+    };
+    fetchData();
+
+  }, []);
+
+  if (!isLoading) return <>Loading...</>
+  if (!data) return <>No Data...</>
+
+  console.log(data);
+
   return (
     <>
-      hsTable
+      <Box className={classes.box} />
+      <Container>
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>year</TableCell>
+                <TableCell>total</TableCell>
+                <TableCell>region</TableCell>
+                <TableCell>outdoor</TableCell>
+                <TableCell>indoor</TableCell>
+              </TableRow>
+            </TableHead>
+            {/* <TableBody>
+            {data.map((row, i) => (
+              <TableRow key={i}>
+                <TableCell>{row}</TableCell>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+              </TableRow>
+            ))}
+          </TableBody> */}
+
+            <TableRow>
+              <TableCell>{data['2020'].year}</TableCell>
+              <TableCell>{data['2020'].total}</TableCell>
+              <TableCell>{data['2020'].region}</TableCell>
+              <TableCell>{data['2020'].otdoor_subtot}</TableCell>
+              <TableCell>{data['2020'].indoor_subtot}</TableCell>
+            </TableRow>
+          </Table>
+        </TableContainer>
+      </Container>
     </>
   )
 }
