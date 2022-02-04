fb0cacd77547e05093f8ccdaf3917d857070ca44
seyoung8239 <seyoung8239@gmail.com>
2022-02-05 00:57:19 +0900

    add table chart

--- frontend/src/pages/HSTable/index.js
+++ frontend/src/pages/HSTable/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import {heatwave_total, heatwave_region} from '../../RouteAPI';
 
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -11,31 +12,68 @@ import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 
+import { Chart as ChartJS } from 'chart.js/auto'
+import { Bar } from 'react-chartjs-2';
+
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+
 import useStyles from './styles';
 
 const HSTable = () => {
   const classes = useStyles();
-  const [data, setData] = useState([]);
+  const [dataTotal, setDataTotal] = useState([]);
+  const [dataRegion, setDataRegion] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [isRowOpen, setIsRowOpen] = useState([]);
+  const [chartData, setChartData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await axios.get('http://34.64.174.66:8000/heatwave/total');
-        setData(res.data);
+        const resTotal = await axios.get(heatwave_total);
+        setDataTotal(resTotal.data);
+        const resRegion = await axios.get(heatwave_region);
+        setDataRegion(resRegion.data);
+        setIsRowOpen(new Array(resTotal.data.length).fill(false));
       } catch (e) {
         console.log(e);
       }
       setIsLoading(true);
     };
     fetchData();
-
   }, []);
 
-  if (!isLoading) return <>Loading...</>
-  if (!data) return <>No Data...</>
+  useEffect(() => {
+    if (dataRegion) {
+      dataRegion.forEach(year => {
+        const lables = year.map(city => city.region);
+        const data = year.map(city => parseInt(city.total));
+        setChartData(prev => prev.concat({ labels: lables, datasets: [{ label: '지역별 온열 질환자 현황', data: data, backgroundColor: "rgba(255, 99, 132, 0.5)" }] }));
+      })
+    }
+  }, [dataRegion]);
 
-  console.log(data);
+  const handleRowClick = (i) => {
+    setIsRowOpen(prev => prev.map((x, index) => {
+      if (index === i)
+        return !x;
+      else
+        return x;
+    }));
+  }
+
+  const options = {
+    responsive: false,
+  };
+
+  if (!isLoading) return <>Loading...</>
+  if (!dataTotal || !dataRegion) return <>No Data...</>
+
+  // console.log(dataTotal);
+  console.log(dataRegion);
+  // console.log(isRowOpen);
+  // console.log(chartData);
 
   return (
     <>
@@ -45,37 +83,38 @@ const HSTable = () => {
           <Table>
             <TableHead>
               <TableRow>
-                <TableCell>year</TableCell>
-                <TableCell>total</TableCell>
-                <TableCell>region</TableCell>
-                <TableCell>outdoor</TableCell>
-                <TableCell>indoor</TableCell>
+                <TableCell>Year</TableCell>
+                <TableCell>Total</TableCell>
+                <TableCell>Outdoor</TableCell>
+                <TableCell>Indoor</TableCell>
+                <TableCell size="small"></TableCell>
               </TableRow>
             </TableHead>
-            {/* <TableBody>
-            {data.map((row, i) => (
-              <TableRow key={i}>
-                <TableCell>{row}</TableCell>
-                <TableCell></TableCell>
-                <TableCell></TableCell>
-                <TableCell></TableCell>
-                <TableCell></TableCell>
-              </TableRow>
-            ))}
-          </TableBody> */}
-
-            <TableRow>
-              <TableCell>{data['2020'].year}</TableCell>
-              <TableCell>{data['2020'].total}</TableCell>
-              <TableCell>{data['2020'].region}</TableCell>
-              <TableCell>{data['2020'].otdoor_subtot}</TableCell>
-              <TableCell>{data['2020'].indoor_subtot}</TableCell>
-            </TableRow>
+            <TableBody>
+              {dataTotal.map((row, i) => (
+                <>
+                  <TableRow key={i} onClick={() => handleRowClick(i)}>
+                    <TableCell>{row.year}</TableCell>
+                    <TableCell>{row.total}</TableCell>
+                    <TableCell>{row.otdoor_subtot}</TableCell>
+                    <TableCell>{row.indoor_subtot}</TableCell>
+                    <TableCell size="small">{isRowOpen[i] ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />}</TableCell>
+                  </TableRow>
+                  {isRowOpen[i] && chartData && <TableCell className={classes.chart} colSpan={5} align="center">
+                    <Bar data={chartData[i]} key={i} options={options} height={200} width={400}/>
+                  </TableCell>}
+                </>
+              ))}
+            </TableBody>
           </Table>
         </TableContainer>
       </Container>
     </>
   )
 }
+
+
+
+
 
 export default HSTable;
